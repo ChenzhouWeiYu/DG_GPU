859df8cda7564f3c8177d713c18a2f57fceb71d1
czwy <yahoo_con_cn@163.com>
2025-10-25 15:03:14 +0800

    20251024

--- examples/euler_2d/double_mach_n60/run.cu
+++ examples/euler_2d/double_mach_n60/run.cu
@@ -178,7 +178,7 @@ void RunCompressibleEuler(uInt N, FilesystemManager& fsm, LoggerSystem& logger){
     // using Flux = HLLCFlux<decltype(physics)>;
     using Flux = StabilizedFlux<HLLCFlux<decltype(physics)>>;
     ExplicitConvectionGPU<decltype(physics), Flux, decltype(condition), Basis::OrderBasis, QuadC, QuadF> convection(physics,condition);
-    PositivityPreservingLimiterGPU<decltype(physics), Basis::OrderBasis, QuadC, QuadF, 1> positive_limiter(gpu_mesh, physics);
+    PositivityPreservingLimiterGPU<decltype(physics), Basis::OrderBasis, QuadC, QuadF, 2> positive_limiter(gpu_mesh, physics);
     PositiveLimiterGPU<Basis::OrderBasis, QuadC, QuadF> positive_limiter_old(gpu_mesh);
     // SamplingPoints<1,
     // typename AutoQuadSelector<1, GaussLegendreTet::Auto>::type, 
