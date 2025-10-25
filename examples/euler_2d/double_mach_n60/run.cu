f1ae598deb18dbe9f85d85f064b654a4ae16ddd9
czwy <yahoo_con_cn@163.com>
2025-10-25 18:43:40 +0800

    20251024

--- examples/euler_2d/double_mach_n60/run.cu
+++ examples/euler_2d/double_mach_n60/run.cu
@@ -175,8 +175,8 @@ void RunCompressibleEuler(uInt N, FilesystemManager& fsm, LoggerSystem& logger){
 
     DoubleMachIBCondition<decltype(physics)> condition(physics);
     // using Flux = LaxFriedrichsFlux<decltype(physics)>;
-    using Flux = HLLFlux<decltype(physics)>;
-    // using Flux = HLLCFlux<decltype(physics)>;
+    // using Flux = HLLFlux<decltype(physics)>;
+    using Flux = HLLCFlux<decltype(physics)>;
     // using Flux = StabilizedFlux<HLLCFlux<decltype(physics)>>;
     ExplicitConvectionGPU<decltype(physics), Flux, decltype(condition), Basis::OrderBasis, QuadC, QuadF> convection(physics,condition);
     PositivityPreservingLimiterGPU<decltype(physics), Basis::OrderBasis, QuadC, QuadF, 1> positive_limiter(gpu_mesh, physics);
