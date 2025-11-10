be7ffd3e1cbb3643c8d8bbff43669961cb0edbd7
喵呜 <yahoo_con_cn@163.com>
2025-11-10 23:44:22 +0800

    1111

--- include/dg/flux_schemes/hllc_flux.h
+++ include/dg/flux_schemes/hllc_flux.h
@@ -63,6 +63,89 @@
 //     }
 // };
 
+
+
+// hll_flux.h
+#pragma once
+#include "dg/flux_schemes/rotated_flux_scheme.h"
+
+template<typename Physics>
+class HLLFlux : public RotatedFluxScheme<HLLFlux<Physics>, Physics> {
+public:
+    using Base = RotatedFluxScheme<HLLFlux<Physics>, Physics>;
+    HostDevice __forceinline__
+    static std::tuple<Scalar,Scalar> compute_SLR(
+        const Physics& physics,
+        const DenseMatrix<5, 1>& U_L,
+        const DenseMatrix<5, 1>& U_R){
+
+        Scalar gamma = physics.get_gamma();
+        Scalar rho_L = Base::positive(U_L[0]), rho_R = Base::positive(U_R[0]);
+        Scalar u_L = U_L[1] / rho_L, u_R = U_R[1] / rho_R;
+        Scalar p_L = physics.compute_pressure(U_L);
+        Scalar p_R = physics.compute_pressure(U_R);
+        Scalar H_L = (U_L[4] + p_L) / rho_L;
+        Scalar H_R = (U_R[4] + p_R) / rho_R;
+
+        // Roe 平均
+        Scalar sqrt_rho_L = sqrt(rho_L);
+        Scalar sqrt_rho_R = sqrt(rho_R);
+        // Scalar rho_tilde = sqrt_rho_L * sqrt_rho_R;
+        Scalar u_tilde = (sqrt_rho_L * u_L + sqrt_rho_R * u_R) / (sqrt_rho_L + sqrt_rho_R);
+        Scalar H_tilde = (sqrt_rho_L * H_L + sqrt_rho_R * H_R) / (sqrt_rho_L + sqrt_rho_R);
+        Scalar a_tilde = sqrt(fmax(0.0, (gamma - 1.0) * (H_tilde - 0.5 * u_tilde * u_tilde)));
+
+        // 波速估计 (Toro 1994)
+        // Scalar S_L = fmin(u_L - a_tilde, u_tilde - a_tilde);
+        // Scalar S_R = fmax(u_R + a_tilde, u_tilde + a_tilde);
+
+        
+        Scalar a_L = physics.compute_sound_speed(U_L);
+        Scalar a_R = physics.compute_sound_speed(U_R);
+        Scalar S_L = fmin(u_L - a_L, u_R - a_R);
+        Scalar S_R = fmax(u_L + a_L, u_R + a_R);
+
+        return std::make_tuple(S_L,S_R);
+    }
+    HostDevice __forceinline__
+    static DenseMatrix<5, 1> compute_1d(
+        const Physics& physics,
+        const DenseMatrix<5, 1>& U_L,
+        const DenseMatrix<5, 1>& U_R) {
+        
+        Scalar gamma = physics.get_gamma();
+        Scalar rho_L = Base::positive(U_L[0]), rho_R = Base::positive(U_R[0]);
+        Scalar u_L = U_L[1] / rho_L, u_R = U_R[1] / rho_R;
+        Scalar p_L = physics.compute_pressure(U_L);
+        Scalar p_R = physics.compute_pressure(U_R);
+        
+        auto [S_L,S_R] = compute_SLR(physics,U_L,U_R);
+
+        auto F_L = physics.compute_flux_1d(U_L);
+        auto F_R = physics.compute_flux_1d(U_R);
+
+        // 消除三段 if 的方法， max min 虽然也是分支，但是是一个轻量的分支
+        Scalar SL0 = std::min(0.0,S_L),   SR0 = std::max(0.0,S_R);
+        return (SR0 * F_L - SL0 * F_R + SL0 * SR0 * (U_R - U_L)) / (SR0 - SL0);
+        // DenseMatrix<5, 1> flux;
+        // if (S_L >= 0.0) {
+        //     flux = F_L;
+        // } else if (S_R <= 0.0) {
+        //     flux = F_R;
+        // } else {
+        //     // HLL 核心公式
+        //     Scalar denom = 1.0 / (S_R - S_L);
+        //     for (uInt i = 0; i < 5; ++i) {
+        //         flux[i] = (S_R * F_L[i] - S_L * F_R[i] + S_L * S_R * (U_R[i] - U_L[i])) * denom;
+        //     }
+        // }
+        // return flux;
+    }
+};
+
+
+
+
 template<typename Physics>
 class HLLCFlux : public RotatedFluxScheme<HLLCFlux<Physics>, Physics> {
 public:
@@ -82,29 +165,8 @@ public:
         Scalar e_L = U_L[4] / rho_L, e_R = U_R[4] / rho_R;
         Scalar p_L = physics.compute_pressure(U_L);
         Scalar p_R = physics.compute_pressure(U_R);
-        Scalar H_L = (U_L[4] + p_L) / rho_L;
-        Scalar H_R = (U_R[4] + p_R) / rho_R;
 
-        // Roe 平均
-        Scalar sqrt_rho_L = sqrt(rho_L);
-        Scalar sqrt_rho_R = sqrt(rho_R);
-        // Scalar rho_tilde = sqrt_rho_L * sqrt_rho_R;
-        Scalar u_tilde = (sqrt_rho_L * u_L + sqrt_rho_R * u_R) / (sqrt_rho_L + sqrt_rho_R);
-        Scalar H_tilde = (sqrt_rho_L * H_L + sqrt_rho_R * H_R) / (sqrt_rho_L + sqrt_rho_R);
-        Scalar a_tilde = sqrt(fmax(0.0, (gamma - 1.0) * (H_tilde - 0.5 * u_tilde * u_tilde)));
-
-        // 波速估计 (Toro 1994)
-        Scalar S_L = fmin(u_L - a_tilde, u_tilde - a_tilde);
-        Scalar S_R = fmax(u_R + a_tilde, u_tilde + a_tilde);
-        
-        // Scalar a_L = physics.compute_sound_speed(U_L);
-        // Scalar a_R = physics.compute_sound_speed(U_R);
-        // Scalar S_L = fmin(u_L - a_L, u_R - a_R);
-        // Scalar S_R = fmax(u_L + a_L, u_R + a_R);
-
-        // 接触间断速度 (Toro 1994)
-        // Scalar S_M = (rho_R * u_R * (S_R - u_R) - rho_L * u_L * (S_L - u_L) + p_L - p_R) /
-        //              (rho_R * (S_R - u_R) - rho_L * (S_L - u_L));
+        auto [S_L,S_R] = HLLFlux<Physics>::compute_SLR(physics,U_L,U_R);
         Scalar S_M = (p_R - p_L + rho_L * u_L * (S_L - u_L) - rho_R * u_R * (S_R - u_R)) /
                     (rho_L * (S_L - u_L) - rho_R * (S_R - u_R));
 
@@ -139,119 +201,6 @@ public:
         else{
             flux = (S_R <= 0.0) ? F_R : F_R + S_R * (UstarR_prime - U_R);
         }
-        // if (S_M >= 0.0) {
-        //     flux = (S_L >= 0.0) ? F_L : physics.compute_flux_1d(UstarL_prime);
-        // }
-        // else{
-        //     flux = (S_R <= 0.0) ? F_R : physics.compute_flux_1d(UstarR_prime);
-        // }
-        // if (S_L >= 0.0) {
-        //     flux = F_L;
-        // } else if (S_M >= 0.0) {
-        //     // 左星号状态 (Toro 1994)
-        //     Scalar rho_L_star = rho_L * (S_L - u_L) / (S_L - S_M);
-        //     Scalar p_L_star = p_L + rho_L * (u_L - S_L) * (u_L - S_M);
-        //     DenseMatrix<5, 1> U_L_star;
-        //     U_L_star[0] = rho_L_star;
-        //     U_L_star[1] = rho_L_star * S_M;
-        //     U_L_star[2] = U_L[2]; // 切向动量不变
-        //     U_L_star[3] = U_L[3];
-        //     U_L_star[4] = p_L_star / (gamma - 1.0) + 0.5 * rho_L_star * S_M * S_M;
-        //     // flux = physics.compute_flux_1d(U_L_star);
-        //     flux = F_L + S_L * (U_L_star - U_L);
-        // } else if (S_R >= 0.0) {
-        //     // 右星号状态 (Toro 1994)
-        //     Scalar rho_R_star = rho_R * (S_R - u_R) / (S_R - S_M);
-        //     Scalar p_R_star = p_R + rho_R * (u_R - S_R) * (u_R - S_M);
-        //     DenseMatrix<5, 1> U_R_star;
-        //     U_R_star[0] = rho_R_star;
-        //     U_R_star[1] = rho_R_star * S_M;
-        //     U_R_star[2] = U_R[2];
-        //     U_R_star[3] = U_R[3];
-        //     U_R_star[4] = p_R_star / (gamma - 1.0) + 0.5 * rho_R_star * S_M * S_M;
-        //     // flux = physics.compute_flux_1d(U_R_star);
-        //     flux = F_R + S_R * (U_R_star - U_R);
-        // } else {
-        //     flux = F_R;
-        // }
         return flux;
-    }
-};
-
-
-// hll_flux.h
-#pragma once
-#include "dg/flux_schemes/rotated_flux_scheme.h"
-
-template<typename Physics>
-class HLLFlux : public RotatedFluxScheme<HLLFlux<Physics>, Physics> {
-public:
-    using Base = RotatedFluxScheme<HLLFlux<Physics>, Physics>;
-
-    HostDevice __forceinline__
-    static DenseMatrix<5, 1> compute_1d(
-        const Physics& physics,
-        const DenseMatrix<5, 1>& U_L,
-        const DenseMatrix<5, 1>& U_R) {
-        
-        // Scalar rho_L = Base::positive(U_L[0]), rho_R = Base::positive(U_R[0]);
-        // Scalar u_L = U_L[1] / rho_L, u_R = U_R[1] / rho_R;
-        // Scalar p_L = physics.compute_pressure(U_L);
-        // Scalar p_R = physics.compute_pressure(U_R);
-
-        // // 波速估计 (Davis)
-        // Scalar a_L = physics.compute_sound_speed(U_L);
-        // Scalar a_R = physics.compute_sound_speed(U_R);
-        // Scalar S_L = fmin(u_L - a_L, u_R - a_R);
-        // Scalar S_R = fmax(u_L + a_L, u_R + a_R);
-
-        // auto F_L = physics.compute_flux_1d(U_L);
-        // auto F_R = physics.compute_flux_1d(U_R);
-        
-        
-        Scalar gamma = physics.get_gamma();
-        Scalar rho_L = Base::positive(U_L[0]), rho_R = Base::positive(U_R[0]);
-        Scalar u_L = U_L[1] / rho_L, u_R = U_R[1] / rho_R;
-        Scalar p_L = physics.compute_pressure(U_L);
-        Scalar p_R = physics.compute_pressure(U_R);
-        Scalar H_L = (U_L[4] + p_L) / rho_L;
-        Scalar H_R = (U_R[4] + p_R) / rho_R;
-
-        // Roe 平均
-        Scalar sqrt_rho_L = sqrt(rho_L);
-        Scalar sqrt_rho_R = sqrt(rho_R);
-        // Scalar rho_tilde = sqrt_rho_L * sqrt_rho_R;
-        Scalar u_tilde = (sqrt_rho_L * u_L + sqrt_rho_R * u_R) / (sqrt_rho_L + sqrt_rho_R);
-        Scalar H_tilde = (sqrt_rho_L * H_L + sqrt_rho_R * H_R) / (sqrt_rho_L + sqrt_rho_R);
-        Scalar a_tilde = sqrt(fmax(0.0, (gamma - 1.0) * (H_tilde - 0.5 * u_tilde * u_tilde)));
-
-        // 波速估计 (Toro 1994)
-        Scalar S_L = fmin(u_L - a_tilde, u_tilde - a_tilde);
-        Scalar S_R = fmax(u_R + a_tilde, u_tilde + a_tilde);
-        
-        // Scalar a_L = physics.compute_sound_speed(U_L);
-        // Scalar a_R = physics.compute_sound_speed(U_R);
-        // Scalar S_L = fmin(u_L - a_L, u_R - a_R);
-        // Scalar S_R = fmax(u_L + a_L, u_R + a_R);
-
-        auto F_L = physics.compute_flux_1d(U_L);
-        auto F_R = physics.compute_flux_1d(U_R);
-
-        // 消除三段 if 的方法， max min 虽然也是分支，但是是一个轻量的分支
-        Scalar SL0 = std::min(0.0,S_L),   SR0 = std::max(0.0,S_R);
-        return (SR0 * F_L - SL0 * F_R + SL0 * SR0 * (U_R - U_L)) / (SR0 - SL0);
-        // DenseMatrix<5, 1> flux;
-        // if (S_L >= 0.0) {
-        //     flux = F_L;
-        // } else if (S_R <= 0.0) {
-        //     flux = F_R;
-        // } else {
-        //     // HLL 核心公式
-        //     Scalar denom = 1.0 / (S_R - S_L);
-        //     for (uInt i = 0; i < 5; ++i) {
-        //         flux[i] = (S_R * F_L[i] - S_L * F_R[i] + S_L * S_R * (U_R[i] - U_L[i])) * denom;
-        //     }
-        // }
-        // return flux;
     }
 };
